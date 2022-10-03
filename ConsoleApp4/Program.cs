0a5b7fe1d8d920ddaa59d88847c67c08613d33b3
- <wavevape@gmail.com>
2022-10-03 22:29:40 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,53 @@
+﻿
+Test t = new Test();
+
+ThreadPool.QueueUserWorkItem(new WaitCallback(t.Method1));
+
+
+void Method2()
+{
+    
+}
+
+
+class Test
+{
+    
+    public void Method1(object a)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Method1");
+            Thread.Sleep(1000);
+        }
+    }
+}
+
+
+class ThreadPoolWorker
+
+{
+
+    public bool Flag { get; set; } = false;
+
+    private readonly Action<object> deleg;
+
+    public ThreadPoolWorker(Action<object> action)
+    {
+        deleg = a;
+    }
+
+    void Start()
+    {
+        ThreadPool.QueueUserWorkItem(new WaitCallback(deleg));
+        Flag = true;
+    }
+
+    public void Wait()
+    {
+
+    }
+
+
+}
+
