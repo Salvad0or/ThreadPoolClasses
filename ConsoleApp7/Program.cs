8cb7ef5c8f8b7be7ae470428fe0fd5f60b38e3d2
- <wavevape@gmail.com>
2022-10-06 22:29:46 +0300

    06.10

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> calculate = MethodAsync(10);
+
+            while (!calculate.IsCompleted)
+            {
+                Console.WriteLine("*");
+                Thread.Sleep(50);
+            }
+
+            calculate.ContinueWith((t) => Console.WriteLine(t.Result));
+
+            Console.ReadKey();
+        }
+
+        static Task<int> MethodAsync(int a)
+        {
+            return Task.Run(() => MethodSum(a));
+        }
+
+        static int MethodSum(int a)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Thread.Sleep(200);
+                a *= i;
+            }
+
+            return a;
+        }
+    }
+}
